e1ee69779fb201d4eb3c12c907b60f15ff32bee3
Yan Zhou <zhouyan87910@gmail.com>
2022-09-03 14:46:01 -0400

    créer la page Login sans style

--- contratsreact/src/App.js
+++ contratsreact/src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Header from "./components/Header";
+import PageLogin from "./components/pages/PageLogin";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
+    const [utilisateur, setUtilisateur] = useState([])
 
-        <div className="App">
-         yan
-        </div>
+    const fetchUtilisateur = async (utilisateur) => {
+        console.log("utilisateur : " + utilisateur.nom)
+        const res = await fetch(`http://localhost:8080/utilisateur/${utilisateur}`)
+        const data = await res.json()
+        setUtilisateur(data)
+        return data
+    }
+
+  return (
+      <Router>
+          <div>
+              <Routes>
+                  <Route exact path='/' element={<PageLogin fechUtilisateur={fetchUtilisateur}/>}/>
+              </Routes>
+          </div>
+      </Router>
+
 
   );
 }
---
+++ contratsreact/src/components/pages/PageLogin.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+import Header from "../Header";
+import Button from "../Button";
+import {Link} from "react-router-dom";
+import {useState} from "react";
+
+const PageLogin = ({fechUtilisateur}) => {
+    const [userName, setUserName] = useState('')
+    const [motDePasse, setMotDePasse] = useState('')
+    console.log(userName)
+    return (
+
+            <div>
+                <form className='add-form' >
+                    <Header title={'Gestion les contrats'} position={'center'}/>
+                    <input type="text" placeholder={'nom'} onChange={(e) => setUserName(e.target.value)} required/>
+                    <input type="text" placeholder={'motDePasse'} onChange={(e) => setMotDePasse(e.target.value)}required/>
+                </form>
+                <Link to={`/ajouterUtilisateur`}><Button text={'Créer un compte'} color={"pink"}/></Link>
+                <Button text={"Connexion"} onClick={()=>fechUtilisateur({nom : userName, motDePasse : motDePasse})}/>
+            </div>
+
+    )
+}
+
+PageLogin.propTypes = {
+    fechUtilisateur: PropTypes.func,
+}
+
+export default PageLogin
